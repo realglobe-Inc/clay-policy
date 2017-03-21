6f815e492ead32685bb35bb20d948185a92bc029
Taka Okunishi <okunishinish@gmail.com>
2017-03-21 14:18:14 +0900

    Add TOC

---
+++ misc/signature.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const jsdoc = require('../jsdoc.json')
+const { snakecase } = require('stringcase')
+
+const paramDesc = ({ parameters = [] }) => parameters
+  .map((param) => param.name)
+  .filter((name) => !/\./.test(name)).join(', ')
+const asClass = (item) => Object.assign({}, item, {
+  functions: (item.functions || []).map((func) => Object.assign(func, {
+    paramDesc: paramDesc(func)
+  })),
+  constructor: Object.assign(item.constructor, {
+    paramDesc: paramDesc(item.constructor)
+  }),
+  instanceName: snakecase(item.name).split('_').pop()
+})
+const asFunc = (item) => Object.assign({}, item, {
+  paramDesc: paramDesc(item)
+})
+const byName = (list, { wrapper = (item) => item }) => list.reduce((result, item) => Object.assign(result, {
+  [item.name]: wrapper(item)
+}), {})
+let classes = byName(jsdoc[ 'classes' ], { wrapper: asClass })
+let { ClayPolicy } = classes
+
+let functions = byName(jsdoc[ 'functions' ], { wrapper: asFunc })
+let {
+  create,
+  isPolicy
+} = functions
+
+module.exports = {
+  classes: [
+    ClayPolicy
+  ].filter(Boolean),
+  functions: [
+    create,
+    isPolicy
+  ].filter(Boolean)
+}
