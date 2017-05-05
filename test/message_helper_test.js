aeb32aadddda6a0bac4da396ba311bd9f2fe2a95
rg-ci <ci@realglobe.jp>
2017-05-05 12:20:23 +0000

    [ci skip] Travis CI committed after build

---
+++ test/message_helper_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for messageHelper.
+ * Runs with mocha.
+ */
+'use strict'
+
+const messageHelper = require('../lib/helpers/message_helper.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('message-helper', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Message helper', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
